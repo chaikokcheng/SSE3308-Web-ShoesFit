cbf0247f8eefa281044592775b3ac66c39666293
Chai Kok Cheng <94617307+chaikokcheng@users.noreply.github.com>
2024-05-05 12:15:33 +0800

    firebase init

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8wgXhWTgQ2O4U6JB4onIVH9mA2Cd__YM",
+  authDomain: "sse3308-shoesfit.firebaseapp.com",
+  databaseURL: "https://sse3308-shoesfit-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "sse3308-shoesfit",
+  storageBucket: "sse3308-shoesfit.appspot.com",
+  messagingSenderId: "28858795897",
+  appId: "1:28858795897:web:42b52dd924dbebb5f0d1a8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
